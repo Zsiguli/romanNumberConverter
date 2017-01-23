e0c68bc320c0c48dad1ab3677a284f6de29fbb48
zsiguli <markovichzsigmond@gmail.com>
2017-01-23 15:31:01 +0100

    Refactoring_1.0
    A made a couple changes based on my new knowledge.

--- converter.js
+++ converter.js
@@ -1,3 +1,9 @@
+var converterArray = [{"1":["I","IV","V","IX"],
+                      "2":["X","XL","L","XC"],
+                      "3":["C","CD","D","CM"],
+                      "4":["M"]}
+                   ];
+
 function convertToRoman(num) {
   var romanNumeral = [];
   var numArr = num.toString().split('');
@@ -7,88 +13,24 @@ function convertToRoman(num) {
     romanNumeral.push(char);
   }
 
-  function fromOneToThree () {
-    switch (numLength) {
-      case 1:
-        romanCharacter ("I");
-        break;
-      case 2:
-        romanCharacter ("X");
-        break;
-      case 3:
-        romanCharacter ("C");
-        break;
-      case 4:
-        romanCharacter ("M");
-        break;
-     }
-  }
-
-  function exactFour () {
-    switch (numLength) {
-      case 1:
-        romanCharacter ("I");
-        romanCharacter ("V");
-        break;
-      case 2:
-        romanCharacter ("X");
-        romanCharacter ("L");
-        break;
-      case 3:
-        romanCharacter ("C");
-        romanCharacter ("D");
-        break;
-    }
-  }
-
-  function exactFive () {
-    switch (numLength) {
-      case 1:
-        romanCharacter ("V");
-        break;
-      case 2:
-        romanCharacter ("L");
-        break;
-      case 3:
-        romanCharacter ("D");
-        break;
-     }
-  }
-
-  function exactNine () {
-    switch (numLength) {
-      case 1:
-        romanCharacter ("I");
-        romanCharacter ("X");
-        break;
-      case 2:
-        romanCharacter ("X");
-        romanCharacter ("C");
-        break;
-      case 3:
-        romanCharacter ("C");
-        romanCharacter ("M");
-        break;
-     }
-  }
-
   for (var i = 0; i<numArr.length; i++) {
     if (numArr[i] < 4) {
       for (var j = 0; j<numArr[i]; j++) {
-        fromOneToThree ();
+        console.log(converterArray[0][numLength][0]);
+        romanNumeral.push(converterArray[0][numLength][0]);
       }
     } else if (numArr[i] < 5) {
        for (var j = 3; j<numArr[i]; j++) {
-        exactFour ();
+        romanNumeral.push(converterArray[0][numLength][1]);
       }
     } else if (numArr[i] < 9) {
-        exactFive ();
+        romanNumeral.push(converterArray[0][numLength][2]);
         for (var j = 5; j<numArr[i]; j++) {
-          fromOneToThree ();
+          romanNumeral.push(converterArray[0][numLength][0]);
       }
     } else if (numArr[i] < 10) {
         for (var j = 8; j<numArr[i]; j++) {
-          exactNine ();
+          romanNumeral.push(converterArray[0][numLength][3]);
       }
     }
     numLength--;
@@ -97,4 +39,4 @@ function convertToRoman(num) {
  return romanNumeral.join('');
 }
 
-convertToRoman(9);
+convertToRoman(12);
